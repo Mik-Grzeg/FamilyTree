f2a1bd43194cd5f6a56ae6dee5d8fa05f26cc00c
Marta Kirsch <martakirsch00@gmail.com>
2021-10-27 11:54:07 +0200

    added colors, fonts and svg used in the project

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "styles/global.styles";
+import theme from "styles/theme";
+import { AppWrapper } from "./App.components";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <AppWrapper>hello</AppWrapper>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
